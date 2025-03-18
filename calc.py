f4e3f20c2dcec933fcf491988c1ba80615c3f1df
yebente <yebente@gmail.com>
2025-03-18 08:29:02 +0300

    operation

--- calc.py
+++ calc.py
@@ -1,3 +1,13 @@
 num1=float(input("Enter first number: "))
 num2=float(input("Enter second number: "))
 operation=input("Enter the operation you want to perform: ")
+if operation=="+":
+    print(num1+num2)    
+elif operation=="-":
+    print(num1-num2)
+elif operation=="*":
+    print(num1*num2)
+elif operation=="/":
+    print(num1/num2)
+else:
+    print("Invalid operation")        
