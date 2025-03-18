cd99aecfc4ed59e97ee34bedd2143fcd9c5135c1
yebente <yebente@gmail.com>
2025-03-18 08:34:18 +0300

    Intro-to-Python-Assignment

--- calc.py
+++ calc.py
@@ -1,13 +1,17 @@
 num1=float(input("Enter first number: "))
 num2=float(input("Enter second number: "))
-operation=input("Enter the operation you want to perform: ")
-if operation=="+":
-    print(num1+num2)    
-elif operation=="-":
-    print(num1-num2)
-elif operation=="*":
-    print(num1*num2)
-elif operation=="/":
-    print(num1/num2)
+operation = input("Enter the operation (+, -, *, or /): ")
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 == 0:
+        result = "Cannot divide by zero"
+    else:
+        result = num1 / num2
 else:
-    print("Invalid operation")        
+    result = "Invalid operation"
+print(f"The result is: {result}")
