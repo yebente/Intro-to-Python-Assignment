367742818061f2f68e9b4ad1685b7700d478d032
yebente <yebente@gmail.com>
2025-03-18 07:49:15 +0300

    Operation input

--- calc.py
+++ calc.py
@@ -1,3 +1,3 @@
 num1=float(input("Enter first number: "))
 num2=float(input("Enter second number: "))
-print("Enter the operation you want to perform")
+operation=input("Enter the operation you want to perform: ")
